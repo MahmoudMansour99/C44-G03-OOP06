5e78058b80063523e4e71494fce1577335c85c8a
Mahmoud Mansour <mansour@gmail.com>
2025-08-15 10:33:32 +0300

    Complete First Project

---
+++ Assignment/First Project/Point3D.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.First_Project
+{
+    internal class Point3D:IComparable<Point3D>, ICloneable
+    {
+        #region Properties
+        public int X { get; }
+        public int Y { get; }
+        public int Z { get; }
+        #endregion
+
+        #region Constructors
+        public Point3D() { }
+        public Point3D(int x, int y, int z)
+        {
+            X= x;
+            Y= y;
+            Z= z;
+        }
+        public Point3D(Point3D other) : this(other.X, other.Y, other.Z) { }
+        #endregion
+
+        public override string ToString()
+        {
+            return $"Point Coordinates: ({X}, {Y}, {Z})";
+        }
+        public static bool operator ==(Point3D left, Point3D right)
+        {
+            if (left is null && right is null)
+                return true;
+
+            if (left is null || right is null )
+                return false ;
+
+            return left.X == right.X && left.Y == right.Y && left.Z == right.Z ;
+        }
+        public static bool operator !=(Point3D left, Point3D right)
+        {
+            return !(left == right);
+        }
+        public int CompareTo(Point3D? other)
+        {
+            if (other is null)
+                return 1;
+
+            int result = X.CompareTo(other.X);
+            if(result != 0 )
+                return result;
+
+            result = Y.CompareTo(other.Y);
+            if(result != 0 ) 
+                return result;
+
+            return Z.CompareTo(other.Z);
+        }
+
+        public object Clone()
+        {
+            return new Point3D(this);
+        }
+    }
+}
---
+++ Assignment/Program.cs
@@ -0,0 +1,85 @@
+﻿using Assignment.First_Project;
+using System.Drawing;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static int ReadCoordinate(string coordinateName)
+        {
+            int value;
+            while (true)
+            {
+                Console.Write($"Enter: {coordinateName}: ");
+                string? input = Console.ReadLine();
+
+                if (int.TryParse(input, out value))
+                    break;
+
+                else
+                    Console.WriteLine("Invalid input. Please enter a valid integer.");
+            }
+            return value;
+        }
+        static void Main(string[] args)
+        {
+            #region First Project
+            // 1. Define 3D Point Class and the basic Constructors (use chaining in constructors).
+
+            // 2.Override the ToString Function to produce this output:
+            // Point3D P = new Point3D(10, 10, 10);
+            // Console.WriteLine(P.ToString());
+            // Output: “Point Coordinates: (10, 10, 10)”.
+
+            // 3. Read from the User the Coordinates for 2 points P1, P2 (Check the input using try Pares, Parse, Convert).
+
+            // 4.	Try to use  ==
+            // If(P1 == P2)   Does it work properly?
+
+            // 5. Define an array of points and sort this array based on X & Y coordinates. 
+
+            // 6. Implement ICloneable interface to be able to clone the object.
+
+            //Point3D point3D = new Point3D(10, 10, 10);
+            //Console.WriteLine(point3D);
+
+            //Console.WriteLine("Enter Coordinates for Point 01: ");
+            //int X1 = ReadCoordinate("X1");
+            //int Y1 = ReadCoordinate("Y2");
+            //int Z1 = ReadCoordinate("Z3");
+            //Point3D P01 = new Point3D(X1, Y1, Z1);
+
+            //Console.WriteLine("Enter coordinates for Point 02: ");
+            //int X2 = ReadCoordinate("X2");
+            //int Y2 = ReadCoordinate("Y2");
+            //int Z2 = ReadCoordinate("Z2");
+            //Point3D P02 = new Point3D(X2, Y2, Z2);
+
+            //Console.WriteLine($"P01 == P02? {P01 == P02}");
+
+            //Point3D[] points = new Point3D[]
+            //{
+            //    new Point3D(3,5,0),
+            //    new Point3D(1,7,2),
+            //    new Point3D(1,2,9),
+            //    new Point3D(3,1,4),
+            //    P01, P02
+            //};
+
+            //Array.Sort(points);
+            //Console.WriteLine("\nSorted Points: ");
+            //foreach (Point3D point in points)
+            //{
+            //    Console.WriteLine(point);
+            //}
+
+            //Point3D cloneP01 = (Point3D)P01.Clone();
+            //Console.WriteLine($"\nCloned P01: {cloneP01}");
+
+            #endregion
+
+
+        }
+    }
+}
